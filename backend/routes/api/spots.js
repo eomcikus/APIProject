2062b6e00a74006f1e0cba4960bc4bfd5023b54e
Erin Omcikus <erinomcikus@gmail.com>
2022-09-27 18:30:43 -0400

    work on spotId route

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Spot } = require('../../db/models');
+const { Booking } = require('../../db/models');
+const { Review } = require('../../db/models');
+const { SpotImage } = require('../../db/models');
+const { ReviewImage } = require('../../db/models');
+const { User } = require('../../db/models');
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
+
+
+router.get('/:spotId', async (req, res, next) => {
+    const spots = await Spot.findByPk(req.params.spotId);
+    const reviewCount = await Review.count({
+        where: {
+            spotId: req.params.spotId
+        }
+    })
+    const reviewSum = await Review.sum('stars', {
+        where:
+        {
+            spotId: req.params.spotId
+        }
+    })
+    const reviewAvg = await (reviewSum / reviewCount)
+
+    const spotImages = await SpotImage.findAll({
+        where: {
+            spotId: req.params.spotId
+        },
+        attributes: ['id', 'url', 'preview']
+    })
+
+    // console.log(spotImages)
+
+    res.json({
+        spots,
+        numReviews: reviewCount,
+        avgStarRating: reviewAvg,
+        SpotImages: spotImages
+    })
+})
+
+router.put('/:spotId', async (req, res) => {
+if (document.cookie = token)
+})
+
+
+
+module.exports = router;
