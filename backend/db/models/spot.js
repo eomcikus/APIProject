77e13d030a8b54f21511c8d226e4d8d06c4ecdaa
Erin Omcikus <erinomcikus@gmail.com>
2022-09-29 12:50:09 -0400

    test

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,12 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Spot.belongsToMany(
-        models.User,
-        {
-          through: models.Booking
-        }
-      )
+      // Spot.belongsToMany(
+      //   models.User,
+      //   {
+      //     through: models.Booking
+      //   }
+      // )
       Spot.hasMany(
         models.SpotImage,
         { foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true }
