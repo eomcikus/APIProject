0bc7b8f3008755ba83803c66bca33cc539b59f13
Erin Omcikus <erinomcikus@gmail.com>
2022-10-14 17:09:28 -0400

    signup button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -15,12 +16,12 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
+        <LoginFormModal />
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
   }
-// console.log('sessionlinks', sessionLinks)
+
   return (
     <ul>
       <li>
