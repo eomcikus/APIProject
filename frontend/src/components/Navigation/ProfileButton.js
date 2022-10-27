85725204e8b77100203c82aec75fb52b49087037
Erin Omcikus <erinomcikus@gmail.com>
2022-10-27 10:24:41 -0400

    css and logout returns to spots

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import { Redirect } from 'react-router-dom'
+import { Redirect, useHistory } from 'react-router-dom'
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  
+  const history = useHistory()
   const openMenu = () => {
     if (showMenu) return;
     setShowMenu(true);
@@ -26,7 +26,8 @@ function ProfileButton({ user }) {
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
-    return <Redirect to="/spots" />
+    return history.push('/spots')
+    // return <Redirect to="/spots" />
   };
 
   return (
@@ -36,6 +37,7 @@ function ProfileButton({ user }) {
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
+          <li>Hello, {user.firstName}</li>
           <li>{user.username}</li>
           <li>{user.email}</li>
           <li>
