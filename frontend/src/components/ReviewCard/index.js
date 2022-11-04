fd5898fd75c70f82c5bf72df03a90d4dc1e9115a
Erin Omcikus <erinomcikus@gmail.com>
2022-11-04 13:50:04 -0400

    reviews formatting

--- frontend/src/components/ReviewCard/index.js
+++ frontend/src/components/ReviewCard/index.js
@@ -3,18 +3,23 @@ import { useSelector, useDispatch } from 'react-redux';
 
 
 const ReviewCard = ({review}) => {
+    console.log('review', review)
     const spot = useSelector(state => state.spots.singleSpot)
     const dispatch = useDispatch()
     return (
         
         <div className='review-container'>
-            <div className='spot-review-info'></div>
+            {/* <div className='spot-review-info'></div> */}
             <div className='review-userName'>
             {review?.User.firstName}
+            <div className='review-content'>
+                {review?.review}
             </div>
-            
+            </div>
         </div>
-        
+        // <div>{review?.review}, {review?.stars} by: {review?.User?.firstName} {review?.User?.lastName}
+        // {sessionUser && sessionUser.id === review.userId && (
+        //     <RemoveReview />
     )
 }
 
