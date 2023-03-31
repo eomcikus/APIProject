7d478421be0da8cb90c187d24a5d167395cad39b
Erin Omcikus <erinomcikus@gmail.com>
2023-03-31 12:00:51 -0400

    clean up code in reviewsforspot

--- frontend/src/components/ReviewsForSpot/index.js
+++ frontend/src/components/ReviewsForSpot/index.js
@@ -13,29 +13,25 @@ const ReviewsForSpot = () => {
     let reviews = useSelector(state => Object.values(state.reviews));
     // console.log('reviews', reviews)
     let reviews2 = Object.values(reviews[0])
-    // console.log('reviews2', reviews2)
-    // console.log('reviews in all for spot', Object.values(reviews2))
-    // let finalArr = reviews.filter(review => +review.spotId === +spotId);
-    // console.log('filteredArr', finalArr)
+
     const sessionUser = useSelector(state => state.session.user);
     const dispatch = useDispatch();
     const reviewObj = useSelector(state => state.reviews);
     const reviewArr = Object.values(reviewObj);
     const spot = useSelector(state => state.spots.singleSpot);
-    // console.log(userReview)
     useEffect(() => {
         dispatch(ReviewsActions.getReviews(spotId))
     }, [dispatch]);
     if (!reviews2) return null
     if (reviews.length === 0) return null;
-    // if (array.length === 0) return null;
+
     return (
         <>
     <div className='spot-avg-reviews'><b>★ {spot.avgStarRating ? parseFloat(spot.avgStarRating).toFixed(2) : 'None'} · {reviews2 ? reviews2.length : 'No'} reviews </b></div><p></p>
         <div className='whole-r-container'>
             <div className='reviews-layout-contain'>
                 <div className='reviews-contained'>{reviews2.map(review => (<div className='review-card'>
-                    <ReviewCard key={review.id} review={review} /></div>))}
+                    <ReviewCard key={review.id} review={review} /></div>))} 
                 </div>
             </div>
         </div>
